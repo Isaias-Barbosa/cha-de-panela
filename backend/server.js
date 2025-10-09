6a3e5882e16bbe751004e593b2f328bca2651f79
Isaias-Barbosa <isaiasbarbosa111@gmail.com>
2025-10-09 17:47:38 -0300

    separando o backend

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,19 @@
 import jsonServer from "json-server";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(process.cwd(), "db.json"));
+const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
+
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
-  console.log(`JSON Server rodando na porta ${port}`);
+  console.log(`✅ JSON Server rodando na porta ${port}`);
 });
